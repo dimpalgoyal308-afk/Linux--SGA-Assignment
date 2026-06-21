301f548235ab1c7385b60b7a2958c271f70cb772
dimpalgoyal308-afk <dimpalgoyal308@gmail.com>
2026-06-22 00:14:13 +0530

    Add files via upload

---
+++ JavaPractice/Passengerdata.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Passenger implements Comparable<Passenger> {
+    private String name;
+    privare String passengerId;
+    private  int age;
+    private String ticketClass;
+    private String startPlace;
+    private String destination;
+    private tring
