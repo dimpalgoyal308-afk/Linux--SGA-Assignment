b29cc1f0242a857605742d7698baa761bfd694ee
dimpalgoyal308-afk <dimpalgoyal308@gmail.com>
2026-06-22 00:24:27 +0530

    Delete JavaPractice directory

--- JavaPractice/Passengerdata.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Passenger implements Comparable<Passenger> {
-    private String name;
-    privare String passengerId;
-    private  int age;
-    private String ticketClass;
-    private String startPlace;
-    private String destination;
-    private tring
